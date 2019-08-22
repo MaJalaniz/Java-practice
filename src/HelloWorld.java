74bf597c0d79a22658ba9531d52cce4acbe3c61d
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-22 13:50:23 -0500

    pushing practice notes

---
+++ src/HelloWorld.java
@@ -0,0 +1,40 @@
+public class HelloWorld {
+        //The public keyword is an access modifier, this allows us to define a scope
+        //class used to define a class
+        //This is the class body
+
+    public static void main(String[] args) {//code block
+        //Methods are collections of statements to perform an operation
+        //The Main method is the entry point of Java code
+
+        String myName = "Matthew"; //These are ways to store information on the computer
+        //                          variables can be changed
+        // Int - integer/whole number without decimal
+
+        int theFirstNum = 12; //Declaration Statements by data type and name
+        //Expressions are constructs that evaluate to a single value.
+
+        int theSecondNum = 10;
+        int theThirdNum = 20;
+        int myTotal = theSecondNum + theThirdNum;
+
+        int myLastOne = myTotal - 1000;
+
+        System.out.println("Hello " + myName);
+        System.out.println("Hello Matthew!" );//This is a String Literal in the Parenthesis
+        System.out.println(theFirstNum);
+        System.out.println(theSecondNum + theThirdNum);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+        //Primitive data types = boolean, byte, char, short, int, long, float and double
+        //building blocks for data manipulation
+
+
+
+    }//code block
+
+
+
+
+}
---
+++ src/primitiveDataTypes.java
@@ -0,0 +1,30 @@
+
+
+public class primitiveDataTypes {
+    public static void main(String[] args) {
+        //Primitive data types = boolean, byte, char, short, int, long, float and double
+        //building blocks for data manipulation
+        //Packages organize Java projects
+
+        // Wrapper class for all primitive data types
+
+        // int  = int myInt = 1,234,567,789; / 32 bits/width
+        // boolean = boolean myBoolean = true/false;
+        // byte = 127 -- -128 saving memory or numbers within this range / 8 bits/width
+        // char char myChar = 'a';
+        // long = L/ 64 bits/width
+        // float
+        // double double = myDouble = 3.4;
+        // short = 32700 -- -32000 / 16 bits/width
+
+        // Putting a larger/smaller value to ints will result into over/under flow
+        // result being the Max or Min number being printed instead
+        // long theLongNum = 123_456_789_123; is treated as an int rather than a long because the lack of L for Long
+
+    }
+
+
+
+
+
+}
