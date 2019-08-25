bcb0b43bbeb92c99a7a9a76a092d8d8e774cbfd8
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-25 09:43:14 -0500

    Access modifiers and methods and classes

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    //OOP part 1
+    //real world objects have two characteristics: state and behavior
+    //state: characteristics i.e ram hd size, age, legs, sleep
+    //behavior: bootup, sounds, drawing, eating drinking, fighting
+    //software objects store their state in fields ie variables, and behaviors with methods
+    //class powerful user defined datatype,
+
+    public static void main(String[] args){
+                car porshe = new car(); //defining an object/creating an object
+                car ford = new car();//
+            porshe.model = "Chevy";
+
+
+
+
+
+    }
+
+
+}
