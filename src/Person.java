03a16a622717eebcdc713728cdbddc8f26f5ef9b
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-02 20:10:56 -0500

    working with constructors

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+    public String firstName;
+    public String lastName;
+    public int age;
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setFirstName(String first){
+        this.firstName = first;
+    }
+    public void setSecondName(String second){
+        this.lastName = second;
+    }
+    public void setAge(int personAge){
+        if(personAge < 100 && personAge > 0){
+            this.age = 0;
+        }else {
+            this.age = personAge;
+        }
+    }
+
+    public boolean isTeen() {
+        if (age > 12 && age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public String getFullName(){
+        if (firstName.isEmpty() && lastName.isEmpty()){
+            return " ";
+        }else if (firstName.isEmpty()){
+            return lastName;
+        }else if(lastName.isEmpty()){
+            return firstName;
+        }else{
+            return firstName + lastName;
+        }
+    }
+
+}
---
+++ src/simpleCalculator.java
@@ -0,0 +1,43 @@
+public class simpleCalculator {
+    public static void main(String[] args){
+
+    }
+
+
+public double firstNumber;
+public double secondNumber;
+
+    public double getFirstNumber(){
+        return this.firstNumber;
+    }
+    public double getSecondNumber(){
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double one){
+        this.firstNumber = one;
+    }
+    public void setSecondNumber(double two){
+        this.secondNumber = two;
+    }
+
+    public double getAdditionResult(){
+        return firstNumber + secondNumber;
+    }
+    public double getSubtractionResult(){
+        return firstNumber - secondNumber;
+    }
+    public double getMultiplicationResult(){
+        return firstNumber * secondNumber;
+    }
+    public double getDivisionResult(){
+        if(secondNumber == 0){
+            return 0;
+        }else{
+            return firstNumber / secondNumber;
+        }
+    }
+
+
+
+}
