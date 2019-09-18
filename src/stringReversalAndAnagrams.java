207a5d1283c77534309a3f35045d1423c2515e2b
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-18 14:51:56 -0500

    string reversal

--- src/stringReversalAndAnagrams.java
+++ src/stringReversalAndAnagrams.java
@@ -1,7 +1,17 @@
+import java.util.*;
+
 public class stringReversalAndAnagrams {
 
     public static void main(String[] args){
 
+
+        String phrase = "This phrase is reversed!";
+
+        StringBuilder phraseOne = new StringBuilder();
+
+        phraseOne.append(phrase);
+        phraseOne = phraseOne.reverse();
+        System.out.println(phraseOne);
 
     }
 
