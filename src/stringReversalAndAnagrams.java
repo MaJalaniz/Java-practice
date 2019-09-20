a962db0952c4c0bb93580a794cfa24f91392a376
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-19 21:28:31 -0500

    fiddling with String Array

--- src/stringReversalAndAnagrams.java
+++ src/stringReversalAndAnagrams.java
@@ -13,6 +13,36 @@ public class stringReversalAndAnagrams {
         phraseOne = phraseOne.reverse();
         System.out.println(phraseOne);
 
+
+        //fiddling with String Arrays
+        String same = "feat";
+        String word = "fate";
+
+        String wordArr[] = word.split(" ");
+
+        for (int i=0; i < wordArr.length; i++){
+            System.out.println(wordArr[i]);
+        }
+
+
+
+       // char[] stringToCharArray = word.toCharArray();
+        char[] stringArray = same.toCharArray();
+
+
+        //System.out.println(stringToCharArray);
+
+       // System.out.println(stringArray);
+        //for (char output : stringToCharArray){
+        //    System.out.println(output);
+       ///\ }
+
+
+
+
+
+
+
     }
 
 }
