11c408cf13761b42fb6b514c6dd6850d2f09dcb3
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-22 19:48:26 -0500

    doing edabit JS challenges in java

---
+++ src/edaBit.java
@@ -0,0 +1,29 @@
+public class edaBit {
+    public static void main(String[] args){
+    System.out.println(Sum(5,4));
+    System.out.println(firstElement(numbersArray));
+
+    }
+
+    //return a sum of two numbers
+private static int Sum(int one, int two) {
+    return one + two;
+}
+
+//=====================================================================
+    //Return the first element in an array
+private static int[] numbersArray = new int[]{100,2,3,4,5,6,7,8,9,10};
+
+private static int firstElement(int[] one){
+    return one[0];
+}
+
+//=====================================================================
+
+
+
+
+
+
+
+}
