cfb3999a7cffc6025469aff7ed3e494e551eb34b
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-23 19:55:00 -0500

    plus three easy challenges

--- src/edaBit.java
+++ src/edaBit.java
@@ -2,9 +2,11 @@ public class edaBit {
     public static void main(String[] args){
     System.out.println(Sum(5,4));
     System.out.println(firstElement(numbersArray));
-
+    System.out.println(area(5,10));
+    System.out.println(nextNum(5));
+    System.out.println(edge(10,11));
     }
-
+//====================================================================
     //return a sum of two numbers
 private static int Sum(int one, int two) {
     return one + two;
@@ -20,10 +22,22 @@ private static int firstElement(int[] one){
 
 //=====================================================================
 
+//Return the area of a triangle
 
+private static int area(int base, int height){
+    return (base * height)/2;
+}
 
+//======================================================================
 
+private static int nextNum(int plusOne){
+    return plusOne + 1;
+}
+//=======================================================================
 
-
+private static int edge(int sideOne, int sideTwo){
+    return (sideOne + sideTwo) - 1;
+}
+//=======================================================================
 
 }
