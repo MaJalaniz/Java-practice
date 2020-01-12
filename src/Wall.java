133bfbd57f481119b21f1c2f77569914495ebaa2
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-11 22:04:59 -0600

    working on  a small java lesson

--- src/Wall.java
+++ src/Wall.java
@@ -1,6 +1,14 @@
+
 public class Wall {
     public static void main(String[] args){
 
+        bank user = new bank(124,500.00, "Joe","jojo@aol.com", 2101993);
+        user.setAccountNumber(150);
+        user.setName("Joseph");
+        user.setEmail("jojostar@gmail.com");
+        System.out.println("The user: "+ user.getName() + ", has " + "$" + user.getBalance() + " in his account.");
+        
+        
     }
 
 
--- src/bank.java
+++ src/bank.java
@@ -2,9 +2,51 @@ public class bank {
     public static void main(String[] args){
       
 
+    }
     private int accountNumber;
     private double balance;
     private String name;
     private String email;
-    private int phoneNumber;
+    private long phoneNumber;
+
+    bank(int accountNumber,double balance, String name, String email, long phoneNumber){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public long getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber = accountNumber;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPhoneNumber(long phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
 }
