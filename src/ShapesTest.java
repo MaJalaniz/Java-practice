28f189968f613c8549e111333af22bf88ae11ddf
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-13 18:53:58 -0600

    Shapes Testing Abstraction Java

---
+++ src/ShapesTest.java
@@ -0,0 +1,16 @@
+class ShapesTest {
+
+public static void main(String[] args){
+
+    Shape s1 = new circle("red", 12.5);
+    Shape s2 = new rectangle(10, 12, "green");
+
+    System.out.println(s1.toString());
+    System.out.println(s2.toString());
+
+}
+
+
+
+
+}
