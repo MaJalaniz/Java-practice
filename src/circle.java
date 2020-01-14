baceeda6af5af10cd6d4c5263ce5ebd818dc1a34
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-13 18:48:43 -0600

    using Shapes to understand Abstraction in Java

--- src/circle.java
+++ src/circle.java
@@ -1,7 +1,22 @@
-class circle extends Shape{
+public class circle extends Shape{
 
+public static void main(String[] args){
 
+}
+double radius;
 
+public circle(String color, double radius){
+    super(color);
+    this.radius = radius;
+}
+@Override
+    double area(){
+        return Math.PI * (radius * 2);
+    }
+ @Override
+    public String toString(){
+        return "Circle color is " + super.color +  
+        "and area is : " + area(); 
+    }
 
-    
 }
---
+++ src/rectangle.java
@@ -0,0 +1,28 @@
+class rectangle extends Shape{
+
+public static void main(String[] args){
+
+}
+
+double length;
+double width;
+
+public rectangle(int length, int width, String color){
+    super(color);
+    this.length = length;
+    this.width = width;
+}
+
+@Override
+double area(){
+    return length * width;
+}
+
+
+@Override
+public String toString(){
+  return  "Circle color is " + super.color +  
+                       "and area is : " + area(); 
+}
+
+}
