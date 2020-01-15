48de6864b9bd64a7b72e83e205ec9b277ffd6253
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-14 19:35:36 -0600

    understanding polymorphism

---
+++ src/calc.java
@@ -0,0 +1,11 @@
+class calc extends calcAdd{
+
+public static void main(String[] args){
+
+    System.out.println(add(4.5, 4.5));
+
+}
+
+
+
+}
---
+++ src/calcAdd.java
@@ -0,0 +1,33 @@
+class calcAdd{
+
+public static void main(String[] args){
+
+
+}
+public static int add(int a, int b){
+    return a + b;
+}
+public static int add(int a, int b, int c){
+    return a + b + c;
+}
+public static int add(int a, int b, int c, int d){
+    return a + b + c + d;
+}
+
+public static double add(double a, double b){
+    return a + b;
+}
+public static double add(double a, double b, double c){
+    return a + b + c;
+}
+public static double add(double a, double b, double c, double d){
+    return a + b + c + d;
+}
+public static int subtract(int a, int b){
+    return a - b;
+}
+public static double subtract(int a, int b){
+    return a - b;
+}
+
+}
