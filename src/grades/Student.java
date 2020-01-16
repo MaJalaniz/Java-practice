1b0a366873ee2a093f411150d7acecc39228a916
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-15 20:25:16 -0600

    created a directory and changed a file

---
+++ src/grades/Student.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.*;
+
+class Student {
+
+    public static void main(String[] args){
+      
+
+    }
+
+    Student(String name, ArrayList<Integer> grades){
+        this.name = name;
+        this.grades = grades;
+
+    }
+
+    private ArrayList<Integer> grades = new ArrayList<>();
+    private String name;
+
+
+
+
+
+
+
+
+
+}
