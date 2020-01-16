e13f35e158adc66246d3375083ea7b38141fe8cf
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-01-15 18:56:14 -0600

    Using add method with ArrayList Java

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,24 @@
-class Student{
+import java.util.ArrayList;
+import java.util.*;
+
+class Student {
 
     public static void main(String[] args){
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(0,2);
+        num.add(4);
+        num.add(6);
+        num.add(8);
+        num.add(10);
 
+        System.out.println(num);
     }
 
 
 
 
-    
+
+
 
 
 }
