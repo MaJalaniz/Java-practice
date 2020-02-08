875efafb59a08c6324fc239b689f5295c278d8a5
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-07 20:42:36 -0600

    find perimeter

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,16 +1,24 @@
 class edabit_Java_warmup{
 public static void main(String[] args){
-
-
+System.out.println(perimeter(2, 4));
+System.out.println(perimeter(2.5, 4.5));
 }
-
-public int time(int minutes){
+/*
+public static int time(int minutes){
     return minutes * 60;
 }
-public double time(double minutes){
+public static double time(double minutes){
     return minutes * 60;
 }
+*/
 
+public static int perimeter(int height, int width){
+    return ((height * 2) + (width * 2));
+}
+public static double perimeter(double height, double width){
+    return ((height * 2) + (width * 2));
+
+}
 
 
 
