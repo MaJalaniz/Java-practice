ba81803a3893ae2c6827c112558f77fdc8d8f24d
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-08 21:28:38 -0600

    Return the last item in an Array Java warm up

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,41 +1,33 @@
-class edabit_Java_warmup{
-public static void main(String[] args){
-System.out.println(empty("Sparrows"));
-System.out.println();
+public class edabit_Java_warmup{
+
+    public int lastElement(int[] one){
+        int laste = one[1];
+          for(int i = 0; i < one.length; i++){
+              if(one[i] <= laste){
+                  laste = one[2];
+                  //System.out.println(laste);
+              }
+          }
+          return laste;
+      }
+      
+public static void main(String args[]){
+    int[] myArray = new int[3];
+    myArray[0] = 12;
+    myArray[1] = 43;
+    myArray[2] = 5;
+   edabit_Java_warmup m = new edabit_Java_warmup();
+
+System.out.println(m.lastElement(myArray) + ": is the last item in this array");
+System.out.println("This is the new Array");
+
 }
+
+//int[] myArray = new int[2];
+ //edabit_Java_warmup m = new edabit_Java_warmup();
+
+
 /*
-//Very Easy Challenges
-
-public static int time(int minutes){
-    return minutes * 60;
-}
-public static double time(double minutes){
-    return minutes * 60;
-}
-public static int perimeter(int height, int width){
-    return ((height * 2) + (width * 2));
-}
-public static double perimeter(double height, double width){
-    return ((height * 2) + (width * 2));
-}
-public static int thirdSide(int sideOne, int sideTwo){
-    if(sideOne < 0 || sideTwo < 0){
-        System.out.println("A side cannot be less than Zero");
-        return 0;
-    }else{
-        return ((sideOne + sideTwo) - 1);
-    }
-}
-public static boolean lessEqualZero(int num){
-    if(num <= 0){
-        System.out.println("Less than or equal to Zero");
-        return true;
-    }else{
-        System.out.println("Greater than Zero");
-        return false;
-    }
-}
-
 //Medium Challenge
 public static int fibo(int shell){
     if(shell < 0) {
@@ -46,15 +38,7 @@ public static int fibo(int shell){
 }
 */
 
-public static String empty(String word){
-    if(word.isEmpty() == true){
-        System.out.println("Nothing");
-        return "Empty String";
-    }else{
-        System.out.println("Words are present.");
-        return word;
-    }
-}
+
 
 
 
