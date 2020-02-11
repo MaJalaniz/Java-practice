7fe91dd84a9b3cc4987117ad76c6537b30da4a8e
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-11 16:09:16 -0600

    Breaking down a tough warm up exercise Java 2

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,15 +8,21 @@ public static void main(String args[]){
  
 System.out.println();
 HashMap<String, String> listNames = new HashMap<String, String>();
+ArrayList<String> arrayNames = new ArrayList<String>();
+
 
 listNames.put("A", "Wheeler");
 listNames.put("B", "Tiamat");
 listNames.put("C", "Sugimoto");
 
+arrayNames.add(listNames.get("A"));
+arrayNames.add(listNames.get("B"));
+arrayNames.add(listNames.get("C"));
+
 System.out.println("Map contents: " + listNames); 
-System.out.println((listNames.get("A")));   
-System.out.println((listNames.get("B")));   
-System.out.println((listNames.get("C")));   
+System.out.println("Array contents: " + arrayNames);   
+System.out.println();   
+System.out.println();   
 
 }
 
