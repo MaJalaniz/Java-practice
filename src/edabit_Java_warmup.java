4cbceeffb3eed72970f98b63b53cb909208a6496
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-10 22:29:23 -0600

    Java Edabit Warm up Recursion Sum

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,40 +1,12 @@
 public class edabit_Java_warmup{
 
-    public int lastElement(int[] one){
-        int laste = one[1];
-          for(int i = 0; i < one.length; i++){
-              if(one[i] <= laste){
-                  laste = one[2];
-                  //System.out.println(laste);
-              }
-          }
-          return laste;
-      }
+
+
 
 public static void main(String args[]){
-    int[] myArray = new int[3];
-    myArray[0] = 12;
-    myArray[1] = 43;
-    myArray[2] = 5;
-   edabit_Java_warmup m = new edabit_Java_warmup();
+ 
+System.out.println(summer(12));
 
-//System.out.println(m.lastElement(myArray) + ": is the last item in this array");
-//System.out.println("This is the new Array");
-System.out.println(evenly(4,3));
-}
-
-//Very Easy challenge
-
-/*
-//Whats wrong with this code?
-
-public static int squaed(int b){
-    return $a * $a
-}
- */
-
-public static int squared(int a){
-    return a * a;
 }
 
  public static int evenly(int a, int b){
@@ -46,6 +18,15 @@ public static int squared(int a){
         return 0;
     }
  }
+
+ public static int summer(int begin){
+     if(begin < 1)
+     return begin;
+     //System.out.println(begin);
+     return (begin) + summer(begin-1);
+     }
+     
+
 
 
 
