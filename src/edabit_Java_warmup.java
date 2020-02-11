3d097388254175bb2736405d650eec3d781ed7c7
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-11 14:52:19 -0600

    Factorials recursion warm up Java

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,11 +1,8 @@
 public class edabit_Java_warmup{
 
-
-
-
 public static void main(String args[]){
  
-System.out.println(summer(12));
+System.out.println(factorial(3));
 
 }
 
@@ -22,13 +19,15 @@ System.out.println(summer(12));
  public static int summer(int begin){
      if(begin < 1)
      return begin;
-     //System.out.println(begin);
+     System.out.println(begin);
      return (begin) + summer(begin-1);
      }
-     
 
-
-
-
-
+     public static long factorial(int one){
+         if(one >= 1)
+         return one * factorial(one-1);
+         System.out.println(one);
+         return 1;
+     }
+    
 }
