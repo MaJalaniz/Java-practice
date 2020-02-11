84350690942aef91d28c241c176f97e558b3ec05
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-11 15:53:51 -0600

    Breaking down a tough warm up exercise Java 1

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -1,33 +1,23 @@
-public class edabit_Java_warmup{
+import java.util.HashMap;
+import java.util.Map;
+
+public class edabit_Java_warmup {
 
 public static void main(String args[]){
  
-System.out.println(factorial(3));
+System.out.println();
+HashMap<String, String> listNames = new HashMap<String, String>();
+
+listNames.put("A", "Wheeler");
+listNames.put("B", "Tiamat");
+listNames.put("C", "Sugimoto");
+
+System.out.println("Map contents: " + listNames); 
+System.out.println((listNames.get("A")));   
+System.out.println((listNames.get("B")));   
+System.out.println((listNames.get("C")));   
 
 }
 
- public static int evenly(int a, int b){
-    if(a % b == 0){
-        System.out.println("This divides evenly");
-        return 1;
-    }else{
-        System.out.println("The answer is: " + a/b);
-        return 0;
-    }
- }
-
- public static int summer(int begin){
-     if(begin < 1)
-     return begin;
-     System.out.println(begin);
-     return (begin) + summer(begin-1);
-     }
-
-     public static long factorial(int one){
-         if(one >= 1)
-         return one * factorial(one-1);
-         System.out.println(one);
-         return 1;
-     }
     
 }
