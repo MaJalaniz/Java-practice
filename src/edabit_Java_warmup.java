93353a4300362900f4df3d46429bc4379b2bf5db
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-12 17:32:57 -0600

    Java warm up Even or Odd

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -4,29 +4,17 @@ import java.util.Map;
 
 public class edabit_Java_warmup {
 
-    //Write a method that takes in a Hashmap and turns it into an ArrayList;
-    public static ArrayList<String> changeOver(HashMap<String,String> myList){
-       
-        ArrayList<String> list = new ArrayList<String>(myList.values());
-        return list;
+public static String evenOdd(int one){
+    if(one % 2 == 0){
+        return "Even";
+    }else{
+        return "Odd";
     }
-
+}
 public static void main(String args[]){
 
-HashMap<String, String> listNames = new HashMap<String, String>();
-ArrayList<String> arrayNames = new ArrayList<String>();
 
-listNames.put("A", "Wheeler");
-listNames.put("B", "Tiamat");
-listNames.put("C", "Sugimoto");
-
-arrayNames.add(listNames.get("A"));
-arrayNames.add(listNames.get("B"));
-arrayNames.add(listNames.get("C"));
-
-System.out.println("Map contents: " + listNames); 
-System.out.println("Array contents: " + arrayNames);   
-System.out.println(changeOver(listNames));   
+System.out.println(evenOdd(146)); 
 
 }
 
