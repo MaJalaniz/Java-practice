474cacc9f4a16673ee314058be4eb2fde73bb416
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-11 21:29:30 -0600

    Break down tough Java warmUp into pieces FINAL

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -5,22 +5,16 @@ import java.util.Map;
 public class edabit_Java_warmup {
 
 
-    public static String changeOver(HashMap<String,String> myList){
-        ArrayList<String> arrName = new ArrayList<String>();
-
-        for (Map.Entry<String, String> entry : myList.entrySet())
-        
-        System.out.println("Value: " + entry.getValue());    
-
-        return "x";
-        
+    public static ArrayList<String> changeOver(HashMap<String,String> myList){
+       
+        ArrayList<String> list = new ArrayList<String>(myList.values());
+        return list;
     }
 
 public static void main(String args[]){
-System.out.println();
+
 HashMap<String, String> listNames = new HashMap<String, String>();
 ArrayList<String> arrayNames = new ArrayList<String>();
-
 
 listNames.put("A", "Wheeler");
 listNames.put("B", "Tiamat");
@@ -33,7 +27,6 @@ arrayNames.add(listNames.get("C"));
 System.out.println("Map contents: " + listNames); 
 System.out.println("Array contents: " + arrayNames);   
 System.out.println(changeOver(listNames));   
-System.out.println();   
 
 }
 
