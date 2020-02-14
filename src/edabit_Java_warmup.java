521049778d12e82939c1e2a8194880147cc88102
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-13 19:57:46 -0600

    working through the problem RPS Java

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -9,42 +9,52 @@ public class edabit_Java_warmup {
 
     public static String rockPaperSci(){
 
+    
+    String[] rPi = {"rock", "paper", "scissors"};
+    int length = rPi.length;
+    int randomNumber = (int)(Math.random() * length);
+    String comChoice = rPi[randomNumber];
+        
     Random rand = new Random();
     Scanner scan = new Scanner(System.in);
 
     int rand_Game = rand.nextInt(4);
     System.out.println("Choose Rock, Paper or Scissors: ");
     String playerOne = scan.next();
+    String comp = comChoice;
+    System.out.println("Comp choice: " + comp);
 
 
-    String[] rPi = {"rock", "paper", "scissors"};
-    int length = rPi.length;
-    int randomNumber = (int)(Math.random() * length);
-
+    
     if(rPi[randomNumber] == "rock" && playerOne == "scissors"){
+        String comWin = "Computer Wins!";
         System.out.println("Computer Wins!");
-        return "Computer Wins";
+        return comWin;
     }else if(rPi[randomNumber] == "paper" && playerOne == "rock"){
+        String comWin = "Computer wins";
         System.out.println("Computer Wins!");
-        return "Computer Wins";
+        return comWin;
     }else if(rPi[randomNumber] == "scissors" && playerOne == "paper"){
+        String comWin = "Computer wins";
         System.out.println("Computer Wins!");
-        return "Computer Wins";
+        return comWin;
     }else if(playerOne == "rock" && rPi[randomNumber] == "scissors"){
+        String playerWin = "Player One Wins!";
         System.out.println("Player One Wins!");
-        return "Player One Wins";
+        return playerWin;
     }else if(playerOne == "paper" && rPi[randomNumber] == "rock"){
+        String playerWin = "Player One Wins!";
         System.out.println("Player One Wins!");
-        return "Player One Wins";
+        return playerWin;
     }else if(playerOne == "scissors" && rPi[randomNumber] == "paper"){
+        String playerWin = "Player One Wins!";
         System.out.println("Player One Wins!");
-        return "Player One Wins";
+        return playerWin;
     }else if(playerOne == rPi[randomNumber]){
         System.out.println("TIE game");
         return "TIE Game";
-    }
-        return rPi[randomNumber] + " and " + playerOne;
-       
+    };
+       return "Player Choice: " + playerOne + ", Comp Choice: " + comp; 
     }
 
 
