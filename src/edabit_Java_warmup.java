67e32bb66b4390dba813d79ec866eb3e3ac04376
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-16 15:17:49 -0600

    Return Max first Java warm up

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -7,65 +7,28 @@ import java.util.Scanner;
 
 public class edabit_Java_warmup {
 
-    public static String rockPaperSci(){
+  //Find the minimum and maximum of an array
 
-        String[] rPi = {"rock", "paper", "scissors"};
-
-        int length = rPi.length;
-        int randomNumber = (int)(Math.random() * length);
-        String comChoice = rPi[randomNumber];
-        
-        Scanner scan = new Scanner(System.in);
-
-       // System.out.println("Play?");
-       // String keepPlaying = scan.next();
-        
-       
-    
-        System.out.println("Choose Rock, Paper or Scissors: ");
-          String playerOne = scan.next();
-         String comp = comChoice;
-         System.out.println("Comp choice: " + comp);
-
-         if(rPi[randomNumber].contains("rock") && playerOne.contains("scissors") == true){
-          String comWin = "Computer Wins!";
-             // System.out.println("Computer Wins!");
-         return comWin;
-        }else if(rPi[randomNumber].contains("paper") && playerOne.contains("rock") == true){
-            String comWin = "Computer wins";
-             // System.out.println("Computer Wins!");
-             return comWin;
-         }else if(rPi[randomNumber].contains("scissors") && playerOne.contains("paper") == true){
-             String comWin = "Computer wins";
-            //System.out.println("Computer Wins!");
-             return comWin;
-         }else if(playerOne.contains("rock") && rPi[randomNumber].contains("scissors") == true){
-            String playerWin = "Player One Wins!";
-            // System.out.println("Player One Wins!");
-             return playerWin;
-         }else if(playerOne.contains("paper") && rPi[randomNumber].contains("rock") == true){
-             String playerWin = "Player One Wins!";
-             //System.out.println("Player One Wins!");
-             return playerWin;
-         }else if(playerOne.contains("scissors") && rPi[randomNumber].contains("paper") == true){
-               String playerWin = "Player One Wins!";
-             // System.out.println("Player One Wins!");
-             return playerWin;
-        }else if(playerOne == rPi[randomNumber] == true){
-              //System.out.println("TIE game");
-             return "TIE Game";
-         }else{
-            return "No"; 
-         }
+  public static int maximum(int[] one){
+      int max = 0;
+        for(int i = 0; i < one.length: i++){
+            if(one[i] < max){
+                max = one[i];
+                System.out.println(max);
+            }
+        }
+      return max;
+  }
     
     
     
-}
+
 
 
 public static void main(String args[]){
 
-System.out.println(rockPaperSci());
+    int[] exampleArr = {20, 10, 43, 67, 100, 3};
+System.out.println();
 
 }
 
