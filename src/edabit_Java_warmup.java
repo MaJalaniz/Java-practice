196bc2d84bdfbe2d7116b13b02c9df4e63365d05
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-24 21:23:16 -0600

    Basic cal warm up Java

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -7,47 +7,28 @@ import java.util.Scanner;
 
 public class edabit_Java_warmup {
 
-  //Find the minimum and maximum of an array
-
-  public static int maximum(int[] one){
-      int max = 0;
-      for(int i = 0; i < one.length; i++){
-          //System.out.println(one[i] + " and max: " + max);
-          if(one[i] > max){
-             max = one[i];
-             //System.out.println("This is the new Max: " + max);
-          }
-      }
-      return max;
-  }
-
-  public static int minimum(int[] two){
-       //Hard coded
-      for(int a = 5; a < two.length; a--){
-        System.out.println(two[a]);
-        if(a == 0 || two[a] < 5){
-            return two[a];
-        }
-      }
-      return 0;
-  }
-    //Find both Min and Max
- public static String findBoth(int[] three){
-     if(three.length >= 5){
-         System.out.println("Min equals: " + minimum(three) + " and  Max equals: " + maximum(three));
-         return "Min equals: " + minimum(three) + " and  Max equals: " + maximum(three);
-     }
-     return "0";
- } 
-    
-    
+    //Write a function that takes in two integers and a char and calculates them
+    public static int basicCal(int one, char two, int three){
+       System.out.println(one + " " + two + " "+ three);
+       if(two == '+'){
+           return one += three;
+       }else if(two == '-'){
+           return one -= three;
+       }else if(two == '*'){
+           return one *= three;
+       }else if(two == '/'){
+           return one /= three;
+       }else{
+           return 0;
+       }
+    }
 
 
 
 public static void main(String args[]){
 
-    int[] exampleArr = {20, 10, 43, 67, 100, 3};
-System.out.println(findBoth(exampleArr));
+   
+System.out.println(basicCal(7, '*', 2));
 
 }
 
