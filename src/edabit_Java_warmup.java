8a5bfe2e5e70217f4c2c4098bdcffdb42524ad96
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-03-04 20:32:34 -0600

    clearing up min max

--- src/edabit_Java_warmup.java
+++ src/edabit_Java_warmup.java
@@ -7,28 +7,11 @@ import java.util.Scanner;
 
 public class edabit_Java_warmup {
 
-    //Write a function that takes in two integers and a char and calculates them
-    public static int basicCal(int one, char two, int three){
-       System.out.println(one + " " + two + " "+ three);
-       if(two == '+'){
-           return one += three;
-       }else if(two == '-'){
-           return one -= three;
-       }else if(two == '*'){
-           return one *= three;
-       }else if(two == '/'){
-           return one /= three;
-       }else{
-           return 0;
-       }
-    }
-
-
 
 public static void main(String args[]){
 
    
-System.out.println(basicCal(7, '*', 2));
+
 
 }
 
